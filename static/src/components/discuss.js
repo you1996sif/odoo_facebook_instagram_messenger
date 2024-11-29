e1431a2e5cc1558b92a26203c9265d9c518380c8
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-29 22:38:18 +0300

    Update discuss.js

--- static/src/components/discuss.js
+++ static/src/components/discuss.js
@@ -1,50 +1,43 @@
 /** @odoo-module **/
 
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
-import { Component, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
-import { onMounted } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+import { ThreadView } from "@mail/core/common/thread_view";
 
 export class PartnerSalesPanel extends Component {
     static template = "odoo_facebook_instagram_messenger.PartnerSalesPanel";
-    
+    static components = { ThreadView };
+
     setup() {
-        this.orm = useService("orm");
         this.threadService = useService("mail.thread");
-        
+        this.orm = useService("orm");
         this.state = useState({
             orders: [],
-            isLoading: false,
-            currentPartnerId: null
+            isLoading: false
         });
-
-        onMounted(() => {
-            this.threadService.subscribe("THREAD_SELECTED", this.onThreadSelected.bind(this));
-        });
+        this.threadService.subscribe("CURRENT_PARTNER_ID_CHANGED", this.onPartnerChanged.bind(this));
     }
 
-    async onThreadSelected(thread) {
-        if (thread?.correspondent) {
-            this.state.currentPartnerId = thread.correspondent.id;
-            await this.loadOrders(this.state.currentPartnerId);
-        }
+    async onPartnerChanged(partnerId) {
+        await this.loadOrders(partnerId);
     }
 
     async loadOrders(partnerId) {
         if (!partnerId) return;
         this.state.isLoading = true;
         try {
-            this.state.orders = await this.orm.searchRead(
-                "sale.order",
-                [["partner_id", "=", partnerId]],
-                ["name", "date_order", "state", "amount_total"]
-            );
+            this.state.orders = await this.orm.searchRead("sale.order", [["partner_id", "=", partnerId]], ["name", "date_order", "state", "amount_total"]);
         } finally {
             this.state.isLoading = false;
         }
     }
 }
 
-registry.category("discuss.side_panel").add("partner_sales", {
+registry.category("mail.discuss.side_panel").add("partner_sales", {
     component: PartnerSalesPanel,
+    icon: "fa-shopping-cart",
+    label: _t("Sales Orders"),
+    order: 15,
 });
