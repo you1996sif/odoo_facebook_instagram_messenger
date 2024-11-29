b2eadb4110f09a29526156d616a2101f2dd6692c
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-29 22:39:16 +0300

    Update discuss.js

--- static/src/components/discuss.js
+++ static/src/components/discuss.js
@@ -4,24 +4,16 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Component, useState } from "@odoo/owl";
-import { ThreadView } from "@mail/core/common/thread_view";
 
 export class PartnerSalesPanel extends Component {
     static template = "odoo_facebook_instagram_messenger.PartnerSalesPanel";
-    static components = { ThreadView };
 
     setup() {
-        this.threadService = useService("mail.thread");
         this.orm = useService("orm");
         this.state = useState({
             orders: [],
             isLoading: false
         });
-        this.threadService.subscribe("CURRENT_PARTNER_ID_CHANGED", this.onPartnerChanged.bind(this));
-    }
-
-    async onPartnerChanged(partnerId) {
-        await this.loadOrders(partnerId);
     }
 
     async loadOrders(partnerId) {
@@ -35,7 +27,7 @@ export class PartnerSalesPanel extends Component {
     }
 }
 
-registry.category("mail.discuss.side_panel").add("partner_sales", {
+registry.category("discuss.side_panel").add("partner_sales", {
     component: PartnerSalesPanel,
     icon: "fa-shopping-cart",
     label: _t("Sales Orders"),
