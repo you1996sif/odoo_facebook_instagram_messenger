49acf2094a02e2e70061bb76106f87d38d507b6d
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-29 22:03:40 +0300

    Update discuss.js

--- static/src/components/discuss.js
+++ static/src/components/discuss.js
@@ -1,9 +1,10 @@
+/** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
+import { Component, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
-export class PartnerSalesPanel extends Component {
+class PartnerSalesPanel extends Component {
     static template = "odoo_facebook_instagram_messenger.PartnerSalesPanel";
     
     setup() {
@@ -14,24 +15,15 @@ export class PartnerSalesPanel extends Component {
         });
     }
 
-    async willStart() {
-        await this.loadOrders();
-    }
-
-    async loadOrders() {
+    async loadOrders(partnerId) {
+        if (!partnerId) return;
         this.state.isLoading = true;
         try {
-            const currentThread = this.messaging?.discuss?.thread;
-            if (currentThread?.correspondent) {
-                const partnerId = currentThread.correspondent.id;
-                this.state.orders = await this.orm.searchRead(
-                    "sale.order",
-                    [["partner_id", "=", partnerId]],
-                    ["name", "date_order", "state", "amount_total"]
-                );
-            }
-        } catch (error) {
-            console.error(error);
+            this.state.orders = await this.orm.searchRead(
+                "sale.order",
+                [["partner_id", "=", partnerId]],
+                ["name", "date_order", "state", "amount_total"]
+            );
         } finally {
             this.state.isLoading = false;
         }
